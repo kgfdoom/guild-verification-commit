cf4d43e979a86eb261a4ed6ef1f8340f3fa0adc4
kgfbd <134832189+kgfdoom@users.noreply.github.com>
2026-03-05 20:43:22 +0600

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:42:28.761Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T14:43:54.889Z
+Commit: Patch API endpoint error handling
