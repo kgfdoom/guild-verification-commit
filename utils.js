3a37632781458a3755f6cf0eb731d9737d6907c3
kgfbd <134832189+kgfdoom@users.noreply.github.com>
2026-03-05 20:44:08 +0600

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:44:14.486Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T14:44:40.811Z
+Commit: Add export functionality for reports
